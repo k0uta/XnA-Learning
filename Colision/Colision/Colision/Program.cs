d3aa677a793250f8b2a27b6e2d275f1a95ac6975
Murilo Fioravanti <lilofc@hotmail.com>
2011-08-01 11:53:26 -0300

    Project to learn more about colisions in XNA.

---
+++ Colision/Colision/Colision/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Colision
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (ColisionGame game = new ColisionGame())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
