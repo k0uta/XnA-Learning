6c3807ac16645ad7cb24d0185665974c4e44afc0
Murilo Fioravanti <lilofc@hotmail.com>
2011-09-08 19:08:21 -0300

    Created and edited the player class

---
+++ FirstSideScroller/FirstSideScroller/FirstSideScroller/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FirstSideScroller
+{
+    class Player:Entity
+    {
+
+        //Some player parameters
+        protected int frameNumber;
+        protected float frameSize;
+
+        //The player constructor
+        public Player(Vector2 pos, Texture2D tex, int numberFrame)
+            : base(pos, tex)
+        {
+
+            //Set the number o frames
+            frameNumber = numberFrame;
+
+            //Calculates the frame size according to the number o frames
+            frameSize = texture.Height / numberFrame;
+
+        }
+        
+        //The Draw method, it's override so it calls the base method too
+        public override void Draw()
+        {
+
+
+
+            base.Draw();
+
+        }
+
+    }
+}
